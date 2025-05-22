bbcab743fa15a4e2cd46e7644b33817fbf4ac05b
CaveNightingale <cavenightingale@foxmail.com>
2025-05-23 02:23:25 +0800

    Add some tests for rendering

--- app/src/main/java/io/github/software/coursework/gui/ChartController.java
+++ app/src/main/java/io/github/software/coursework/gui/ChartController.java
@@ -9,6 +9,16 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
+/**
+ * A controller for rendering charts on a canvas.
+ * <pre><code>
+ *  To use this controller, you need to:
+ *      1. use fx:controller="io.github.software.coursework.gui.ChartController"
+ *      2. call setModel() to set the model
+ *      3. call setView() to set a view that is able to render the model with a {@link ChartRendering} object
+ * </code></pre>
+ * @param <T> the type of the model used for rendering
+ */
 public final class ChartController<T> {
     public interface View<T> {
         void onRender(ChartRendering rendering, T model);
@@ -29,18 +39,26 @@ public final class ChartController<T> {
     private T model;
     private View<T> view;
 
+    /**
+     * Set the data model to be rendered.
+     * @param model the model to be rendered
+     */
     public void setModel(T model) {
         this.model = model;
         render();
     }
 
+    /**
+     * Set the view that is able to render the model.
+     * @param view the view that is able to render the model
+     */
     public void setView(View<T> view) {
         this.view = view;
         render();
     }
 
     @FXML
-    public void initialize() {
+    private void initialize() {
         canvasWidth.bind(root.widthProperty());
         canvasHeight.bind(root.heightProperty());
         canvas.widthProperty().bind(canvasWidth);
@@ -68,7 +86,7 @@ public final class ChartController<T> {
         }
     }
 
-    public void render() {
+    private void render() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
